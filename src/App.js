89936343d2c8e19729a9c03ac1b5dda84bd4f889
aldrich27dev <129347457+aldrich27dev@users.noreply.github.com>
2024-04-05 16:28:40 +0800

    myquiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState}from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+  const [currentQuestion, setcurrentQuestions] = useState(0);
+  const [showScore, setshowScore] = useState(false);
+  const [score, setscore] = useState(0);
+
+  // questions
+
+  const allQuestions = [
+    {
+      text: 'Who is Aldrich?' ,
+      options: [
+        {answerText: 'Maangas', isCorrect: true},
+        {answerText: 'Gwapo', isCorrect: false},
+        {answerText: 'Talentado', isCorrect: false},
+      ],
+    },
+    {
+      text: 'Who is Iris Claudette Ablaza?' ,
+      options: [
+        {answerText: 'Maganda', isCorrect: false},
+        {answerText: 'Pretty', isCorrect: false},
+        {answerText: 'Gorgeous', isCorrect: true},
+      ],
+    },
+    {
+      text: 'Sino ang asawa ni Iris?' ,
+      options: [
+        {answerText: 'Molly', isCorrect: false},
+        {answerText: 'Aldrich', isCorrect: true},
+        {answerText: 'Laptop', isCorrect: false},
+      ],
+    },
+    {
+      text: 'Si molly ay isang?' ,
+      options: [
+        {answerText: 'Makulit', isCorrect: false},
+        {answerText: 'Shih Tzu', isCorrect: false},
+        {answerText: 'Aso', isCorrect: true},
+      ],
+    },
+  ]
+  
+  const handleAnswerOptions = (isCorrect) => {
+if (isCorrect) {
+  setscore(score + 1);
 }
+const handleNextQuestion = currentQuestion + 1;
+if (handleNextQuestion < allQuestions.length) {
+  setcurrentQuestions(handleNextQuestion);
+  } else {
+    setshowScore(true); }
+  }
+
+
+
+
+  return (
+    <div className='flex w-full h-screen justify-center items-center bg-gradient-to-r from-yellow-300 to to-white-400'>
+      <div className='bg-slate-100 p-12 rounded-lg shadow-2xl w-full max-w-xl'>
+       
+        {
+          showScore ? (
+            <>
+        <div className='text-slate-700 text-xl font-semibold'>
+          You scored {score} out of {allQuestions.length}
+        </div>
+        <br></br>
+        <button className='m-2 h-10 w-24 rounded-md bg-slate-200 hover:bg-slate-300 hover:shadow-xl transition-all duration-800'onClick={() => {window.location.reload()}}>Ulit!</button>
+        </>
+        ) : (
+<>
+        <div className='text-slate-700 text-xl font-semibold'>
+          <div className='m-4'>
+            <span>Question {currentQuestion + 1}</span>/ {allQuestions.length}
+          </div>
+          <div className='m-4'>
+            {allQuestions[currentQuestion].text}
+          </div>
+          <div className='w-full'>
+              {
+                allQuestions[currentQuestion].options.map((options) => (
+                  <button className='m-2 h-10 w-24 rounded-md bg-slate 300 hover:bg-slate-400 hover:shadow-xl transition-all duration-500 ease-in-out' onClick={()=>handleAnswerOptions(options.isCorrect)}>{options.answerText}</button>
+                ))
+              }
+
+          </div>
+        </div>
+        </>
+        )
+}
+
+      </div>
+  
+    </div>
+    
+  )
+}
+
 
 export default App;
